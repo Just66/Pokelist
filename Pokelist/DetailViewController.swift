3053282614381680000ad7e9a48101335685c258
Dmytro Aprelenko <d.aprelenko@gmail.com>
2017-02-08 19:47:02 +0200

    + Search +Design of the details

---
+++ Pokelist/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Pokelist
+//
+//  Created by Dmytro Aprelenko on 08.02.17.
+//  Copyright © 2017 Dmytro Aprelenko. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var  pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pokelist/ViewController.swift
+++ Pokelist/ViewController.swift
@@ -9,25 +9,30 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var musicPlayer: AVAudioPlayer!
     var pokemon = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
+    var inSearchMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        searchBar.delegate = self
         
+        searchBar.returnKeyType = UIReturnKeyType.done
         parsePokemonCSV()
         initAudio()
     }
     
     func initAudio() {
-        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        let path = Bundle.main.path(forResource: "music.mp3", ofType: nil)!
         
         do {
             musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
@@ -64,9 +69,24 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        var poke: Pokemon!
+        
+        if inSearchMode {
+            poke = filteredPokemon[indexPath.row]
+            
+        } else {
+            poke = self.pokemon[indexPath.row]
+        }
+        
+        performSegue(withIdentifier: "DetailVC", sender: poke)
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if inSearchMode {
+            return filteredPokemon.count
+        }
+        
         return pokemon.count
     }
     
@@ -77,7 +97,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? PokeCell {
             
-            let pokemon = self.pokemon[indexPath.row]
+            var pokemon: Pokemon!
+            if inSearchMode {
+                pokemon = self.filteredPokemon[indexPath.row]
+                cell.configureCell(pokemon)
+                
+            } else {
+                pokemon = self.pokemon[indexPath.row]
+                cell.configureCell(pokemon)
+            }
             cell.configureCell(pokemon)
             
             return cell
@@ -90,6 +118,46 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return CGSize(width: 105, height: 105)
     }
     
-    @IBOutlet weak var musicBtnPressed: UIButton!
-}
+    @IBAction func musicBtn(_ sender: UIButton) {
+        if musicPlayer.isPlaying {
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            collectionView.reloadData()
+            view.endEditing(true)
+        }
+        else
+        {
+            inSearchMode = true
+            
+            let lower = searchBar.text!.lowercased()
+            
+            filteredPokemon = pokemon.filter({$0.name.range(of: lower) != nil}) //we take the original pokemon list then filter it weather search bar text is inluded into the range and $0 is just the placeholder
+            collectionView.reloadData()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailVC" {
+            if let detailsVC = segue.destination as? DetailViewController {
+                if let poke = sender as? Pokemon {
+                    detailsVC.pokemon = poke
+                }
+            }
+        }
+    }
+    
+    }
 
